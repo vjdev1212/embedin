fd9f105202c89c99f15ba4f2c4ddd0d354397dd1
Jarvis <home80jarvis@gmail.com>
2025-02-27 09:00:13 +0530

    Update default embed

--- constants/Embed.ts
+++ constants/Embed.ts
@@ -1,7 +1,7 @@
 //Identitifiers required: IMDBID, SEASON and EPISODE in the URL. 
 
-export const defaultMovieUrlTemplate = "https://vidsrc.cc/v2/embed/movie/{IMDBID}?poster=true&autoPlay=false";
-export const defaultSeriesUrlTemplate = "https://vidsrc.cc/v2/embed/tv/{IMDBID}/{SEASON}/{EPISODE}?poster=true&autoPlay=false";
+export const defaultMovieUrlTemplate = "https://vidsrc.su/embed/movie/{TMDBID}";
+export const defaultSeriesUrlTemplate = "https://vidsrc.su/embed/tv/{TMDBID}/{SEASON}/{EPISODE}";
 export const defaultSandboxAllowed = true;
 
 // export const movieUrlTemplate = "https://vidsrc.to/embed/movie/{IMDBID}";
