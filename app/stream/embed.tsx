5e9c10fa28e6ea1ffada6600973e075abf862561
Jarvis <home80jarvis@gmail.com>
2025-11-01 23:16:11 +0530

    Update userAgent

--- app/stream/embed.tsx
+++ app/stream/embed.tsx
@@ -241,7 +241,7 @@ const EmbedPlayer = () => {
               marginTop: 30,
               marginBottom: 10
             }}
-            userAgent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15"
+            userAgent={navigator.userAgent}
             javaScriptEnabled
             domStorageEnabled
             startInLoadingState
